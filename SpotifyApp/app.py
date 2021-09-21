3637ad81145b4a1cc48779ff01f2cb3adeff5cd9
JosiahLC <86324031+JosiahLC@users.noreply.github.com>
2021-09-21 10:43:23 -0600

    Update app.py

--- SpotifyApp/app.py
+++ SpotifyApp/app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 def create_app():
     app = Flask(__name__)
     
-    @app.route("/",  methods=["GET"])
+    @app.route("/")
     def home():
         return "Hello World"
     
